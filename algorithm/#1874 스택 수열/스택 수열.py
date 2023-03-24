4c40e0022a6079e9f15f0ed3c79a62abc13b78fe
ksy3432 <120364372+ksy3432@users.noreply.github.com>
2023-03-24 15:43:09 +0900

    [1874] Title: 스택 수열, Time: 4048 ms, Memory: 32840 KB -NKLCBHub

---
+++ algorithm/#1874 스택 수열/스택 수열.py
@@ -0,0 +1,37 @@
+n = int(input())
+stack_array = []
+s = []
+val = 1
+res = 0
+
+
+def push(x):
+    global val
+    stack_array.append(x)
+
+
+for i in range(n):
+    find = int(input())
+
+    '''if i == 1 :
+        for j in range(1,find+1):
+            push(j)
+            print('+')'''
+
+    while val <= find:
+        push(val)
+        s.append('+')
+        val += 1
+
+    if find == stack_array[-1]:
+        stack_array.pop()
+        s.append('-')
+
+    else:
+        res = 1
+
+if res == 0:
+    for i in s:
+        print(i)
+else:
+    print('NO')
