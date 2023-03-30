1730a8f3d39415c86edbe8d62c4646ad3f12b1bf
ksy3432 <120364372+ksy3432@users.noreply.github.com>
2023-03-30 17:08:40 +0900

    [1427] Title: 소트인사이드, Time: 68 ms, Memory: 31256 KB -NKLCBHub

--- algorithm/#1427 소트인사이드/소트인사이드.py
+++ algorithm/#1427 소트인사이드/소트인사이드.py
@@ -1,13 +1,39 @@
-import sys
+import sys       #merge_sort
 
-#n = list(map(str, input().split()))
-n = str(sys.stdin.readline())
+n = str(sys.stdin.readline())       #s[]에 원소 넣기
 s = []
 
 for i in range(0, len(n)-1):
     s.append(int(n[i]))
 
-s.sort(reverse=True)
 
-for i in s:
+#s = [9,9,9,9,9,8,9,9,9]
+
+def merge_sort(s):
+    global res
+    if len(s) <= 1:
+        return s
+
+    mid = len(s) // 2
+    left = merge_sort(s[:mid])
+    right = merge_sort(s[mid:])
+
+    res = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] > right[j]:
+            res.append(left[i])
+            i += 1
+        else:
+            res.append(right[j])
+            j += 1
+
+    res += left[i:]
+    res += right[j:]
+            
+    return res
+
+
+res = merge_sort(s)
+for i in res:
     print(i, end='')
